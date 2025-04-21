7a78c2ea612e0bdcf90d845a54a2e471b535288d
biyonik <ahmet.altun60@gmail.com>
2025-04-21 20:57:22 +0300

    Created board component and styles implemented

---
+++ components/board.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "@/styles/board.module.css";
+import { nanoid } from "nanoid";
+
+const Board: React.FC = (): React.ReactNode => {
+  return (
+    <div className={styles.board}>
+      <div className={styles.grid}>
+        {Array.from({ length: 1 }).map(() => {
+          return Array.from({ length: 4 }).map(() => (
+            <div key={nanoid()} className={styles.cell}></div>
+          ));
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Board;

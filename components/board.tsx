28c36a8ea25ec6bf65e36ee391abd3885f065a71
biyonik <ahmet.altun60@gmail.com>
2025-04-22 22:35:30 +0300

    Test structure implemented and Tile component created with styles

--- components/board.tsx
+++ components/board.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import styles from "@/styles/board.module.css";
 import { nanoid } from "nanoid";
+import Tile from "./tile";
 
 const Board: React.FC = (): React.ReactNode => {
   return (
     <div className={styles.board}>
+      <div className={styles.tiles}>
+        <Tile />
+      </div>
       <div className={styles.grid}>
-        {Array.from({ length: 1 }).map(() => {
+        {Array.from({ length: 4 }).map(() => {
           return Array.from({ length: 4 }).map(() => (
             <div key={nanoid()} className={styles.cell}></div>
           ));
---
+++ components/tile.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "@/styles/tile.module.css";
+
+const Tile: React.FC = (): React.ReactNode => {
+  return <div className={styles.tile}>2</div>;
+};
+
+export default Tile;
